8337017c95e989770fb7464a2ef8943b552632a1
varodv <varodv@gmail.com>
2025-02-11 19:21:30 +0100

    Change project name(s)

--- src/main.test.ts
+++ src/main.test.ts
@@ -2,6 +2,6 @@ import { name } from './main';
 
 describe('main', () => {
   it('name is exported', () => {
-    expect(name).toEqual('template-ts');
+    expect(name).toEqual('wordcraft');
   });
 });
--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,1 @@
-export const name = 'template-ts';
+export const name = 'wordcraft';
